0062c75230fc4abe64fff61115b0d642179b79cd
PMS <paul@sweazey.com>
2021-04-25 15:14:49 +0200

    Made button red

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
             Button("Show Modal") {
                 self.isPresented = true
             } // button
+            .foregroundColor(.red)
             .sheet(isPresented: $isPresented) {
                 ModalView()
             .navigationBarTitle("Xcode and Git")
